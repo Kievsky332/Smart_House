ef75c71fdbabfea271102e9a8d28c11b3022a963
kievsky332 <falileevry@gmail.com>
2025-11-05 19:24:05 +0300

    hh

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function myCode(){
 }
 const intervalId = setInterval(myCode, 1000);
 
-function open(){
+function open1(){
     alert("Вы открыли дверь ");
 }
 function up(){
@@ -17,6 +17,17 @@ function up(){
       alert("Машина проехала")
     }, 2000);
 }
-function five(){
+function five(){       
+    const myElement1 = document.getElementById('main');
+    const myElement = document.getElementById('video');
     alert("Спасибо всем ");
+    setTimeout(() => {
+        myElement.style.display = 'block';
+        myElement1.style.display = "none"    
+        setTimeout(() => {
+             myElement1.style.display = "block"
+             myElement.style.display="none"
+        }, 5000);
+    }, 2000);
+
 }
