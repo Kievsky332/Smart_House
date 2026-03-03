1df041e0b7c8e8d3e84bdd7c16401794432e4d57
Kievsky332 <falileevry@gmail.com>
2026-03-03 23:16:52 +0300

    обновление

--- main.js
+++ main.js
@@ -14,33 +14,40 @@ function open1(){
 function up(){
     alert("Шлагбаум поднялся ");
     setTimeout(() => {
-      alert("Машина проехала")
+      alert("Машина проехала");
     }, 2000);
 }
 function five(){       
     const myElement1 = document.getElementById('main');
     const myElement = document.getElementById('video');
-    alert("Спасибо всем ");
     setTimeout(() => {
         myElement.style.display = 'block';
-        myElement1.style.display = "none"    
+        myElement1.style.display = "none"; 
     }, 2000);    
     setTimeout(() => {
-         myElement1.style.display = "block"
-        myElement.style.display="none"
-    }, 5000);
+         myElement1.style.display = "block";
+        myElement.style.display="none";
+    }, 2800);
 }
 function oplata(){
     const my = document.getElementById('cif');
     
-    alert("Вы оплатили "+ my.innerHTML) 
-    summa = 0 
+    alert("Вы оплатили "+ my.innerHTML) ;
+    summa = 0 ;
 }
-let summa = 0
+let summa = 0;
 function myTask() {
     const my = document.getElementById('cif');
-    my.innerHTML =summa+" ₽"
-    summa +=100
+    my.innerHTML =summa/1000+" ₽";
+    summa +=1;
 
 }
 let timerId = setInterval(myTask, 1000);
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'ArrowDown') {
+    five();
+  }
+  else{
+    console.log(event.code);
+  };
+});
